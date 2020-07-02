15358b334060817424873b03eadc486dd0ee74da
Mohammad <mohamadnet@gmail.com>
2020-07-02 11:26:06 +1000

    logo screen

--- app/src/main/java/com/kassh/detectme/MainActivity.java
+++ app/src/main/java/com/kassh/detectme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kassh.detectme;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,49 +9,52 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+    //Hook
+    View topLogo;
+    TextView middleText, slogan;
+    // Animation Variables
+    Animation topAnimation, bottomAnimation, middleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Hooks
+        topLogo = findViewById(R.id.top_lines);
+        middleText = findViewById(R.id.detect_line);
+        slogan = findViewById(R.id.bottom_line);
+
+        topLogo.setAnimation(topAnimation);
+        middleText.setAnimation(middleAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this, secondActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, SPLASH_TIME_OUT);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
