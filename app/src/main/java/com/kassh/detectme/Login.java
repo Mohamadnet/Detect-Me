44af42814bac07f1f3e251d0214b6d9ae2065b75
Mohammad <mohamadnet@gmail.com>
2020-07-13 16:54:33 +1000

    Log in

--- app/src/main/java/com/kassh/detectme/Login.java
+++ app/src/main/java/com/kassh/detectme/Login.java
@@ -1,33 +1,56 @@
 package com.kassh.detectme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import static android.app.ActivityOptions.makeSceneTransitionAnimation;
 
 public class Login extends AppCompatActivity {
-
+    Button callSignUp, login_btn;
+    ImageView image;
+    TextView logoText, sloganText;
+    TextInputLayout username, password;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //This Line will hide the status bar from the screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
-
-        View callSignUp = findViewById(R.id.signup_screen);
-
-        callSignUp.setOnClickListener(new View.OnClickListener(){
+        //Hooks
+        callSignUp = findViewById(R.id.signup_screen);
+        image = findViewById(R.id.logo_image);
+        logoText = findViewById(R.id.logo_name);
+        sloganText = findViewById(R.id.slogan_name);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login_btn = findViewById(R.id.login_btn);
+        callSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-            }
-
-            @override
-            Intent intent = new Intent(Login.this, SingUp.class);
-
-            void startActivity(intent) {
-
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                Pair[] pairs = new Pair[7];
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+                pairs[1] = new Pair<View, String>(logoText, "logo_text");
+                pairs[2] = new Pair<View, String>(sloganText, "logo_desc");
+                pairs[3] = new Pair<View, String>(username, "username_tran");
+                pairs[4] = new Pair<View, String>(password, "password_tran");
+                pairs[5] = new Pair<View, String>(login_btn, "button_tran");
+                pairs[6] = new Pair<View, String>(callSignUp, "login_signup_tran");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this, pairs);
+                startActivity(intent, options.toBundle());
             }
         });
     }
